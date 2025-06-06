9e0c125dabd811bd292b43b90ef88aea4248e7fd
sopyanbadawi <sopyanbadawi.com>
2025-06-06 14:03:46 +0700

    base

--- TransaksiPengisian.java
+++ TransaksiPengisian.java
@@ -1,6 +1,6 @@
 public class TransaksiPengisian {
-    Kendaraan kendaraan = new Kendaraan(null, null, null);
-    BBM bbm = new BBM(null, 0);
+    Kendaraan kendaraan;
+    BBM bbm;
     double liter, totalBayar;
 
     TransaksiPengisian(Kendaraan kendaraan, BBM bbm, double liter) {
